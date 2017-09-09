7f3da1216bf640bab99bce2c3c756f1ae91b401c
runphilrun <pjl7651@rit.edu>
2017-09-08 18:30:22 -0700

    init

---
+++ predict.py
@@ -0,0 +1,18 @@
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load in
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+print('hello')
+
+# Any results you write to the current directory are saved as output.
+
+# h(x) is the predcition
+# x is the value of a variable
+# each variable has coefficient c
+# y is the known output for a given set of variables values
