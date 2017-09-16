cebe48bad4f01447aee791ccfc39c39103583ed6
runphilrun <pjl7651@rit.edu>
2017-09-16 01:11:43 -0700

    data vis humidity vs rad

--- explore.py
+++ explore.py
@@ -11,15 +11,43 @@ import seaborn as sns # advanced plotting tools
 
 def humidity(df):
     '''Plot humidity vs radiation over time'''
-    # df_month = df.groupby('Month')
-    # df_day = df.groupby('Date')
-    # df_hour = df.groupby('Hour')
     plt.figure(figsize=(18, 6))
-    plt.subplot(131)
-    sns.barplot(x='Hour',y='Humidity',data=df)
-    plt.subplot(132)
-    sns.barplot(x='Month',y='Humidity',data=df)
-    plt.subplot(133)
+    plt.subplot(121) # hourly
+    df_hourly = df.groupby('Hour')
+    h_humidity = df_hourly['Humidity'].mean()
+    h_humidity_errorpos =  h_humidity+df_hourly['Humidity'].std()/2
+    h_humidity_errorneg =  h_humidity-df_hourly['Humidity'].std()/2
+    plt.plot(h_humidity)
+    plt.fill_between(range(24), h_humidity_errorpos.values, h_humidity_errorneg.values, alpha=0.3, antialiased=True)
+    h_rad = df_hourly['Radiation'].mean()
+    h_rad *= 1/h_rad.max()*100 # normalize to percent
+    h_rad_errorpos =  (h_rad+df_hourly['Radiation'].std()/2) * 1/h_rad.max()*100 # normalize to percent
+    h_rad_errorneg =  (h_rad-df_hourly['Radiation'].std()/2) * 1/h_rad.max()*100 # normalize to percent
+    plt.plot(h_rad,'r')
+    plt.fill_between(range(24), 0, h_rad, alpha=0.3, antialiased=True, color='red')
+    plt.ylim((0,101))
+    plt.legend(), plt.xlabel('Hour of Day (Local Time)'), plt.ylabel('Percent')
+    plt.title('Average Daily Humidity vs. Average Daily Radiation')
+
+    plt.subplot(122) # daily
+    df_daily = df.groupby('Day')
+    d_humidity = df_daily['Humidity'].mean()
+    d_humidity_errorpos =  d_humidity+df_daily['Humidity'].std()/2
+    d_humidity_errorneg =  d_humidity-df_daily['Humidity'].std()/2
+    plt.plot(d_humidity)
+    plt.fill_between(df['Day'].unique(), d_humidity_errorpos.values, d_humidity_errorneg.values, alpha=0.3, antialiased=True)
+    d_rad = df_daily['Radiation'].mean()
+    d_rad *= 1/d_rad.max()*100 # normalize to percent
+    d_rad_errorpos =  (d_rad+df_daily['Radiation'].std()/2) * 1/d_rad.max()*100 # normalize to percent
+    d_rad_errorneg =  (d_rad-df_daily['Radiation'].std()/2) * 1/d_rad.max()*100 # normalize to percent
+    plt.plot(d_rad,'r')
+    plt.fill_between(df['Day'].unique(), 0, d_rad, alpha=0.3, antialiased=True, color='red')
+    plt.ylim((0,101))
+    plt.legend(), plt.xlabel('Day of Year'), plt.ylabel('Percent')
+    plt.title('Daily Humidity vs. Daily Radiation')
+
+    plt.figure() # bivariate density
     sns.jointplot(x='Humidity',y='Radiation',data=df,kind='kde',cmap='coolwarm',n_levels=30,shade=True)
+    plt.title('Radiation vs. Humidity Bivariate Density')
     sns.plt.show()
-    # https://stackoverflow.com/questions/32899463/how-can-i-overlay-two-graphs-in-seaborn
+    plt.show()
