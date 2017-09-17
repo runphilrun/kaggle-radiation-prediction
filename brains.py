c402a7d8c855089234080dbfd446644520d39fdf
runphilrun <pjl7651@rit.edu>
2017-09-17 02:55:46 -0700

    prep for ml

---
+++ brains.py
@@ -0,0 +1,8 @@
+'''The brain of the Solar Radiation Predictive Algorithm
+
+Philip Linden
+September 17, 2017
+'''
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import sklearn # ML tools
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 '''Execute solar radiation prediction machine learning algorithm.
-This function has no arguments.
 
 Philip Linden
 September 10, 2017
@@ -14,6 +13,7 @@ from bokeh.models import Range1d  # interactive plotting tools
 ## IMPORT FUNCTIONS
 import preprocessing
 import explore
+import brains
 
 ## INGEST DATA
 df, units = preprocessing.ingest_data('input/SolarPrediction.csv')
