dfe99f6ee6ff9eeadb8f6b433a66dcd251731b10
runphilrun <pjl7651@rit.edu>
2017-09-24 02:40:19 -0700

    implement various ml algorithms

--- brains.py
+++ brains.py
@@ -11,31 +11,61 @@ from sklearn.model_selection import train_test_split # split data
 from bokeh.plotting import figure, show, output_notebook
 
 def plot_test(clf,X_test,y_test):
+    ''' Plot trained algorihm against test inputs.
+    inputs:
+        clf         as classifier   the trained algorithm
+        X_test      as array        training data, unordered
+        y_test      as array        outputs corresponding to training data
+    '''
     y_predicted = clf.predict(X_test)
 
-    p = figure(tools='pan,box_zoom,reset',x_range=[0, 100], title='Model validation',y_axis_label='radiation')
+    p = figure(toolbar_location='right',x_range=[0, 100], title='Model validation',y_axis_label='radiation')
     p.grid.minor_grid_line_color = '#eeeeee'
 
     p.line(range(len(y_test)),y_test,legend='actual',line_color='blue')
     p.line(range(len(y_test)),y_predicted,legend='prediction',line_color='red')
-    output_notebook()
+    # output_notebook()
     show(p)
     return
 
-def train_model(df,clf,debug=False):
+def plot_real(clf,x,y_actual,index):
+    ''' Plot predictions for actual measurements.
+    inputs:
+        clf         as classifier   the trained algorithm
+        x           as array        timeseries of measurement inputs
+        y_actual    as array        corresponding timeseries of actual results
+    '''
+    y_predicted = clf.predict(x)
+
+    p = figure(toolbar_location='right', title='Predicted vs Actual',x_axis_label='time', y_axis_label='radiation',x_axis_type="datetime")
+    p.grid.minor_grid_line_color = '#eeeeee'
+
+    p.line(index,y_actual,legend='actual',line_color='blue')
+    p.line(index,y_predicted,legend='prediction',line_color='red')
+    # output_notebook()
+    show(p)
+    return
+
+def train_model(X,y,clf,debug=False):
     ''' Train algorithm.
     inputs:
-        df      as DataFrame
-        alg     as string (if unrecognized, use default)
+        X       as array        features
+        y       as array        label(s)
+        clf     as sci-kit learn classifier (untrained)
     returns:
         clf     as trained classifier
         accuracy  as float
     '''
-    X = df.drop('Radiation',axis=1).as_matrix()
-    y = df['Radiation'].as_matrix()
-    X =  preprocessing.scale(X)
     X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
     model = clf.fit(X_train,y_train)
     accuracy = clf.score(X_test,y_test)
-    if debug: plot_test(clf,X_test,y_test)
     return clf, model, accuracy, X_test, y_test
+
+def go(x,y,algorithm,debug=True):
+    ''' Easy model train and test. '''
+    clf, model, accuracy, X_test, y_test=train_model(x,y,algorithm,debug=True)
+    print('Accuracy: %s percent'%str(accuracy*100))
+
+    plot_test(clf,X_test,y_test)
+    plot_real(clf,x,y,df.index.values)
+    return
--- main.py
+++ main.py
@@ -7,7 +7,12 @@ September 10, 2017
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt # plotting tools
-from sklearn.linear_model import LinearRegression # Linear regression ML algorithm
+
+# IMPORT ML CLASSIFIERS
+from sklearn.linear_model import LinearRegression # Linear regression
+from sklearn.ensemble import RandomForestRegressor # random forest regression
+from sklearn.neural_network import MLPRegressor # neural network regression
+from sklearn.svm import SVR # support vector regression
 
 ## IMPORT FUNCTIONS
 import preprocessing, explore, brains
@@ -21,7 +26,7 @@ plt.figure(figsize=(7,7))
 explore.corrPairs(df)
 df.drop(['WindDirection','WindSpeed'], axis=1, inplace=True) # drop irrelevant features
 
-### EXPLORE data
+## EXPLORE DATA
 feature_list=['Radiation','Humidity','Temperature','Pressure']
 for feature in feature_list[1:]: # radiation vs feature
     plt.figure(figsize=(18, 6))
@@ -33,5 +38,10 @@ plt.show()
 
 df['TimeOfDay'] = df.index.hour # add time of day to correlation
 
-clf, model, accuracy, X_test, y_test=brains.train_model(df,LinearRegression(),debug=True)
-print('Accuracy: %s percent'%str(accuracy*100))
+## IMPLEMENT ALGORITHM
+algorithm = RandomForestRegressor()
+
+x = df.drop('Radiation',axis=1).as_matrix()
+y = df['Radiation'].as_matrix()
+
+brains.go(x,y,algorithm,debug=True)
